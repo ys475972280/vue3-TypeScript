4d28e8b586f0e2eac14964e088903c91350033c0
yangshuai <475972280@qq.com>
2022-08-05 17:56:04 +0800

    feat: 学习

---
+++ src/views/main/system/user/user.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user">
+    <div class="search"></div>
+    <div class="content"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "user",
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
